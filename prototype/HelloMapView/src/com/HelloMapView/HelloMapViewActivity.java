4468ab63f1b243bf041529fbb1c28d3091e73990
Kevin Frame <kevinframe@gmail.com>
2011-09-26 21:25:39 -0400

    android prototype initial commit

---
+++ prototype/HelloMapView/src/com/HelloMapView/HelloMapViewActivity.java
@@ -0,0 +1,28 @@
+package com.HelloMapView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.maps.*;
+import android.widget.*;
+
+public class HelloMapViewActivity extends MapActivity {
+    /** Called when the activity is first created. */
+	LinearLayout linearLayout;
+	MapView mapView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+            }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+  
+    
+    
+ 
+}
