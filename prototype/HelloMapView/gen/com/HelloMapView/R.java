72ff1cdb166b5ab0ef6e2f573537fa55f993f278
Kevin Frame <kevinframe@gmail.com>
2011-09-29 20:06:12 -0400

    added xml overlay

--- prototype/HelloMapView/gen/com/HelloMapView/R.java
+++ prototype/HelloMapView/gen/com/HelloMapView/R.java
@@ -16,6 +16,10 @@ public final class R {
     public static final class id {
         public static final int mainlayout=0x7f050000;
         public static final int mapview=0x7f050001;
+        public static final int quickContactBadge1=0x7f050003;
+        public static final int ratingBar1=0x7f050005;
+        public static final int relativeLayout1=0x7f050002;
+        public static final int spinner1=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
