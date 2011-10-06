7908c2edde1a8bf454bd38923ed369552e1d7c82
Nicholas Martin <npmartin3@gmail.com>
2011-10-06 18:47:14 -0400

    adding more test sql files

---
+++ src/eltoraz/pug/server/UserAddition.sql
@@ -0,0 +1,5 @@
+Insert into users (first_name, last_name) values ('Nick', 'Martin');
+Insert into users (first_name, last_name) values ('Brian', 'Orecchio');
+Insert into users (first_name, last_name) values ('Kevin', 'Frame');
+Insert into users (first_name, last_name) values ('Bill', 'Jameson');
+
