492d4e0179874a8336b9e3395550211879bf1b30
Nicholas Martin <npmartin3@gmail.com>
2011-10-06 18:47:14 -0400

    adding more test sql files

---
+++ src/eltoraz/pug/server/GameAddition.sql
@@ -0,0 +1,2 @@
+Insert into games (sport, descr, creator, location) values('Baseball', 'Fun, easy game', 1, 1);
+Insert into games (sport, creator, location) values('Basketball', 3, 2);
---
+++ src/eltoraz/pug/server/LocationAddition.sql
@@ -0,0 +1,2 @@
+Insert into locations (name, lat, longi) values ('Awesome Park', 42.74739, -73.666592);
+Insert into locations (lat, longi) values (42.269179, -74.476318);
