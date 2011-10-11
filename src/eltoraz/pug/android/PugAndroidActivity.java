a92da9a2d70595d8203bfcc5e655407854333adb
Bill Jameson <hothousevt@gmail.com>
2011-10-10 22:46:27 -0400

    testing new API key hiding methods with git

--- src/eltoraz/pug/android/PugAndroidActivity.java
+++ src/eltoraz/pug/android/PugAndroidActivity.java
@@ -6,23 +6,25 @@ import com.google.android.maps.*;
 import android.widget.*;
 
 public class PugAndroidActivity extends MapActivity {
-    /** Called when the activity is first created. */
 	LinearLayout linearLayout;
 	MapView mapView;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        mapView = (MapView) findViewById(R.id.mapview);
-        mapView.setBuiltInZoomControls(true);
-            }
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
-  
-    
-    
- 
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+
+
+
+
 }
