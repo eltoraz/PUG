385a7b092765bc700c5b41dd43128abee98aa8ae
Bill Jameson <hothousevt@gmail.com>
2011-10-11 01:51:42 -0400

    added Activity for game creation pane & started to tie it in with main activity (PugAndroidActivity)

---
+++ src/eltoraz/pug/android/CreateGameActivity.java
@@ -0,0 +1,7 @@
+package eltoraz.pug.android;
+
+import android.app.Activity;
+
+public class CreateGameActivity extends Activity {
+	
+}
--- src/eltoraz/pug/android/PugAndroidActivity.java
+++ src/eltoraz/pug/android/PugAndroidActivity.java
@@ -1,20 +1,24 @@
 package eltoraz.pug.android;
 
-import android.app.Activity;
 import android.os.Bundle;
 import com.google.android.maps.*;
 import android.widget.*;
 
 public class PugAndroidActivity extends MapActivity {
-	LinearLayout linearLayout;
-	MapView mapView;
+	private LinearLayout linearLayout;
+	private MapView mapView;
+	
+	private Button createGameButton;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		// define functionality for the buttons
+		createGameButton = (Button) findViewById(R.id.create);
+		
 		mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
 	}
