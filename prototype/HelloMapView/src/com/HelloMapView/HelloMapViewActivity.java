ccf39cd8a3644e50340f464c9e8b4a0bc23b57e6
Kevin Frame <kevinframe@gmail.com>
2011-10-11 19:37:47 -0400

    more prototype experimentation

--- prototype/HelloMapView/src/com/HelloMapView/HelloMapViewActivity.java
+++ prototype/HelloMapView/src/com/HelloMapView/HelloMapViewActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.google.android.maps.*;
 import android.widget.*;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class HelloMapViewActivity extends MapActivity {
     /** Called when the activity is first created. */
@@ -16,6 +19,16 @@ public class HelloMapViewActivity extends MapActivity {
         setContentView(R.layout.main);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+       
+        Button next = (Button) findViewById(R.id.button3);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Profile.class);
+                startActivityForResult(myIntent, 0);
+                startActivity(myIntent);
+            }
+
+        });
             }
     @Override
     protected boolean isRouteDisplayed() {
---
+++ prototype/HelloMapView/src/com/HelloMapView/Profile.java
@@ -0,0 +1,30 @@
+package com.HelloMapView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import android.app.Activity;
+import com.google.android.maps.*;
+import android.widget.*;
+import com.google.android.maps.MapView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Profile extends Activity {
+
+    /** Called when the activity is first created. */
+	LinearLayout linearLayout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+        
+            }
+  
+    
+	
+}
