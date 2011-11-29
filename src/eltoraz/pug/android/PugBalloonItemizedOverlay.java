2de291f06d00bb0c194f567a28e7a4915aeadfbf
Brian Orecchio <oreccb@gmail.com>
2011-11-29 17:01:00 -0500

    updated comments on PugBalloonItemizedOverlay

--- src/eltoraz/pug/android/PugBalloonItemizedOverlay.java
+++ src/eltoraz/pug/android/PugBalloonItemizedOverlay.java
@@ -9,22 +9,43 @@ import android.widget.Toast;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
-//public class PugBalloonItemizedOverlay<Item extends OverlayItem> extends BalloonItemizedOverlay<OverlayItem> {
+/**
+ * This class is the custom overlay built on the Open Source <code>BalloonItemizedOverlay</code>
+ * @author oreccb
+ *
+ *
+ */
 public class PugBalloonItemizedOverlay extends BalloonItemizedOverlay<OverlayItem> {
 
 	private ArrayList<OverlayItem> m_overlays = new ArrayList<OverlayItem>();
 	private Context c;
 	
+	/**
+	 * Constructor 
+	 * 
+	 * @param defaultMarker
+	 * @param mapView
+	 */
 	public PugBalloonItemizedOverlay(Drawable defaultMarker, MapView mapView) {
 		super(boundCenter(defaultMarker), mapView);
 		c = mapView.getContext();
 	}
 
+	/**
+	 * gets the Overlay at index i
+	 * 
+	 * @return Overlay at index i
+	 */
 	@Override
 	protected OverlayItem createItem(int i) {
 		return m_overlays.get(i);
 	}
 
+	/**
+	 * This method returns the size of the Overlay ArrayList
+	 * 
+	 * @return integer - which is the size of the Overlay ArrayList
+	 */
 	@Override
 	public int size() {
 		return m_overlays.size();
@@ -37,12 +58,11 @@ public class PugBalloonItemizedOverlay extends BalloonItemizedOverlay<OverlayIte
 	}
 	
 	/**
-	 * Override this method to handle a "tap" on a balloon. By default, does nothing 
-	 * and returns false.
+	 * This method handles a "tap" on a balloon. 
 	 * 
 	 * @param index - The index of the item whose balloon is tapped.
 	 * @param item - The item whose balloon is tapped.
-	 * @return true if you handled the tap, otherwise false.
+	 * @return true 
 	 */
 	@Override
 	protected boolean onBalloonTap(int index, OverlayItem item) {
